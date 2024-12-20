53e423e5ff5c1143bc4d3231445f0adb0b6ada2a
Moorthy Venkatraman <moorthy.sv@gmail.com>
2024-12-20 00:08:50 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Modified to verify the build pipeline!
         </p>
         <a
           className="App-link"
