6303111a71c5ea2b0c0de5c347689f2ed608c218
Moorthy Venkatraman <moorthy.sv@gmail.com>
2024-12-20 00:42:29 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Let's see if Github Actions and Vercel Deploy play nice!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     dev report compiling...
     </div>
   );
 }
